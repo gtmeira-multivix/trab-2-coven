ba7e2a577cb547dc6bd238a9126d9c3695666d7b
Marcellina <43495251+TiagoEU@users.noreply.github.com>
2018-10-27 09:57:49 -0300

    Update arvore.c

--- arvore.c
+++ arvore.c
@@ -8,14 +8,51 @@ struct _arvore {
   Candidato cand;
 };
 
-Arvore* arvore_inserir(Arvore *a, Candidato cand) {
-  //Implemente
+Arvore* arvore_inserir(Arvore *a, Candidato cand) 
+{
+	if(a ==NULL)
+	{
+		Arvore *novo = (Arvore*) malloc (sizeof(Arvore));
+		novo->cand = cand;
+		novo->esquerda = NULL;		
+		novo->direita = NULL;
+		return novo;
+	}
+	if(cand.numero < (a->cand.numero))
+	{
+		a->esquerda = arvore_inserir(a->esquerda, cand);
+	} 
+	else 
+	{
+		a->direita = arvore_inserir(a->direita, cand);
+	}
+	return a;
 }
 
-Candidato* arvore_buscar(Arvore *a, int candNumero) {
-  //Implemente
+Candidato* arvore_buscar(Arvore *a, int candNumero) 
+{
+	if(a == NULL) 
+	{
+		return NULL;
+	}
+	if(a->cand.numero == candNumero)
+	{
+		return &a->cand;
+	}
+	if(candNumero < (a->cand.numero))
+	{
+		return arvore_buscar(a->esquerda, candNumero);
+	} 
+	else 
+	{
+		return arvore_buscar(a->direita, candNumero);
+	}
 }
 
-void arvore_imprime_em_ordem(Arvore *a) {
-  //Implemente
+void arvore_imprime_em_ordem(Arvore *a) 
+{
+	if(a->esquerda != NULL)
+	{
+		arvore_imprime_em_ordem(a->esquerda);
+	}
 }
